7c38e4747a32925faca587119dca839d34c0ebf3
anupbista <bistaanup77@gmail.com>
2020-09-03 09:11:55 +0545

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4a52f9dffa4504ef35a033bd8462f2d5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b14805e6-6af1-449e-bd86-2e77c14aa905'
+};
